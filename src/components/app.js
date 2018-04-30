f879913633304b8c68e3e48bebbf8631e438764b
Omer <omerfu@gmail.com>
2018-04-30 11:14:02 -0700

    initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,9 @@
+import React from 'react';
 
-
+function app() {
+    return(
+        <div className='container'>
+            <h1 className='text-center'>Address Book</h1>
+        </div>
+    );
+}
